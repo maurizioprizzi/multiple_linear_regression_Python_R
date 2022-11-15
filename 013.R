3cc3dc0a72208d2778d95b58e96561a160604451
Maurizio Prizzi <maurizioprizzi@gmail.com>
2022-11-15 20:20:03 -0300

    First steps

---
+++ 013.R
@@ -0,0 +1,16 @@
+# Multiple Linear Regression
+
+# Import the dataset
+dataset = read.csv("50_Startups.csv")
+
+# Encoding categorical data
+dataset$State = factor(dataset$State, 
+                       levels = c("New York", "California", "Florida"), 
+                       labels = c(1, 2, 3))
+
+# Splitting the dataset
+library(caTools)
+set.seed(123)
+split = sample.split(dataset$Profit, SplitRatio = 0.8)
+training_set = subset(dataset, split == TRUE)
+test_set = subset(dataset, split == FALSE)
